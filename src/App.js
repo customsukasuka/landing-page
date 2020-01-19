4d6d027411a087684f78f9877a59b3f2d62f7969
unknown <ariezncahyo@gmail.com>
2020-01-19 11:31:17 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello, Im custom suka suka official web</h1>
+        </div>
+    )
+}
+
+export default App;
